f7ea5970d2c70536fa778fe6aa12f5be4cfedcbd
Truong Dao <ton.truong.dao@outlook.com>
2021-03-21 13:28:42 -0400

    Gia update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import GlobalStyles from "./styles/GlobalStyles";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Home from './pages/Home';
+import GameRoom from './pages/GameRoom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { auth } from './services/firebase';
 
-function App() {
+//  ########## Helper Function ##########
+
+function PrivateRoute({ component: Component, authenticated, ...rest }) {
   return (
+    <Route
+      {...rest}
+      render={(props) => authenticated === true
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
+    />
+  )
+}
+
+function PublicRoute({ component: Component, authenticated, ...rest }) {
+  return (
+<<<<<<< Updated upstream
     <>
       <div className="App">
         <header className="App-header">
@@ -24,6 +48,58 @@ function App() {
       <GlobalStyles />
     </>
   );
+=======
+    <Route
+      {...rest}
+      render={(props) => authenticated === false
+        ? <Component {...props} />
+        : <Redirect to='/chat' />}
+    />
+  )
+}
+
+// #######################################
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false,
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          loading: false,
+        });
+      }
+    })
+  }
+
+  render() {
+    return this.state.loading === true ? <h2>Loading...</h2> : (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <PrivateRoute path="/chat" authenticated={this.state.authenticated} component={GameRoom}></PrivateRoute>
+          <PublicRoute path="/signup" authenticated={this.state.authenticated} component={Signup}></PublicRoute>
+          <PublicRoute path="/login" authenticated={this.state.authenticated} component={Login}></PublicRoute>
+        </Switch>
+      </Router>
+    );
+  }
+
+>>>>>>> Stashed changes
 }
 
 export default App;
